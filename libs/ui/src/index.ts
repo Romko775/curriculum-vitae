ff21ac095725efd13acbd422862890546f6746a6
Roman Yanush <roman.yanush7@gmail.com>
2024-02-27 09:15:10 +0200

    chore: save progress

---
+++ libs/ui/src/index.ts
@@ -0,0 +1,2 @@
+export * from './lib/content-block/content-block.component';
+export * from './lib/stack-badge/stack-badge.component'
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,58 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
-import { ContentBlockComponent } from '../../libs/ui/src/lib/content-block/content-block.component';
+import { ContentBlockComponent } from '@curriculum-vitae/ui';
+import { DatePipe, KeyValuePipe, NgForOf } from '@angular/common';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule, ContentBlockComponent],
+  imports: [NxWelcomeComponent, RouterModule, ContentBlockComponent, NgForOf, KeyValuePipe, DatePipe],
   selector: 'curriculum-vitae-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'curriculum-vitae';
+
+  readonly midDateFormat = 'MMM YYYY';
+  readonly shortDateFormat = 'YYYY';
+
+  readonly technicalSkills = [
+    {
+      title: 'Frontend',
+      skills: [
+        'JavaScript, TypeScript, OOP',
+        'Angular (v4-11), Vue.js (v2), jQuery, Ajax',
+        'CSS (LESS, SCSS), Bootstrap, Material UI',
+        'Grid, Flexbox',
+        'NPM',
+        'RxJS',
+        'Adaptive, Responsive'
+      ]
+    },
+    {
+      title: 'Backend',
+      skills: [
+        'Python 3, Django (inc. DRF)',
+        'NodeJS, Express, Nest JS',
+        'PostgreSQL, MySQL, SQLite'
+      ]
+    },
+    {
+      title: 'Utils',
+      skills: [
+        'Docker, Docker-compose',
+        'Nginx',
+        'Git',
+        'Jira, Confluence, Trello'
+      ]
+    },
+    {
+      title: 'Some experience in',
+      skills: [
+        'Electron.js, Cordova, Ionic',
+        'Flutter, Dart'
+      ]
+    }
+  ];
 }
